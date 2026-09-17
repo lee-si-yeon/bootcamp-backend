deead2314ad6241f9a8c75276de894fe85813587
leesiyeon <sylee4880@naver.com>
2026-09-17 17:33:15 +0900

    feat: 게시글 작성 응답에 보안 팁 추가

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -1,6 +1,7 @@
 package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.domain.MemberPolicy;
 import com.back.global.exception.DomainException;
 import com.back.boundedContext.member.out.MemberRepository;
 import com.back.global.rsData.RsData;
@@ -16,6 +17,7 @@ public class MemberFacade {
 
     private final MemberRepository memberRepository;
     private final MemberJoinUseCase memberJoinUseCase;
+    private final MemberPolicy memberPolicy;
 
     @Transactional(readOnly = true)
     public long count() {
@@ -35,5 +37,9 @@ public class MemberFacade {
     @Transactional(readOnly = true)
     public Optional<Member> findById(int id) {
         return memberRepository.findById(id);
+    }
+
+    public String getRandomSecureTip() {
+        return "비밀번호의 유효기간은 %d일 입니다.".formatted(memberPolicy.getNeedToChangePasswordDays());
     }
 }

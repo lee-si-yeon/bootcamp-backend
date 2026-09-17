c0e93b78e5c150aab7ad81a23d6510fac2ddf4ac
leesiyeon <sylee4880@naver.com>
2026-09-17 14:51:48 +0900

    refactor: 이벤트 기반 활동 점수 처리로 분리

--- src/main/java/com/back/boundedContext/post/entity/Post.java
+++ src/main/java/com/back/boundedContext/post/entity/Post.java
@@ -2,10 +2,13 @@ package com.back.boundedContext.post.entity;
 
 import com.back.global.jpa.entity.BaseIdAndTime;
 import com.back.boundedContext.member.entity.Member;
+import com.back.shared.post.dto.PostCommentDto;
+import com.back.shared.post.event.PostCommentCreatedEvent;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
@@ -17,6 +20,7 @@ import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @NoArgsConstructor
+@Getter
 public class Post extends BaseIdAndTime {
 
     @ManyToOne(fetch = LAZY)
@@ -40,7 +44,8 @@ public class Post extends BaseIdAndTime {
         PostComment postComment = new PostComment(this, author, content);
 
         comments.add(postComment);
-        author.increaseActivityScore(1);
+
+        publishEvent(new PostCommentCreatedEvent(new PostCommentDto(postComment)));
 
         return postComment;
     }

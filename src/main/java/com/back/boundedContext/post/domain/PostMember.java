6a96823b0ce8bbee28c92a72b38ddb4031839495
leesiyeon <sylee4880@naver.com>
2026-09-18 11:17:19 +0900

    feat: 회원 정보 변경 이벤트 기반 동기화 추가

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "post_member")
 public class PostMember extends ReplicaMember {
 
-    public PostMember(int id, LocalDateTime createDate, LocalDateTime modifyDate, String username, String password, String nickname) {
-        super(id, createDate, modifyDate, username, password, nickname);
+    public PostMember(int id, LocalDateTime createDate, LocalDateTime modifyDate, String username, String password, String nickname, int activityScore) {
+        super(id, createDate, modifyDate, username, password, nickname, activityScore);
     }
 }

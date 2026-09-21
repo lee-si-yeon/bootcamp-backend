c81d58f7fdff49c6828456686892c7a01166c626
leesiyeon <sylee4880@naver.com>
2026-09-21 15:33:10 +0900

    feat: 주문 생성과 초기 주문 데이터 추가

---
+++ src/main/java/com/back/boundedContext/market/domain/OrderItem.java
@@ -0,0 +1,39 @@
+package com.back.boundedContext.market.domain;
+
+import com.back.global.jpa.entity.BaseIdAndTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "order_item")
+public class OrderItem extends BaseIdAndTime {
+
+    @ManyToOne(fetch = LAZY)
+    private Order order;
+
+    @ManyToOne(fetch = LAZY)
+    private Product product;
+
+    private String productName;
+
+    private long price;
+
+    private long salePrice;
+
+    private double payoutRate = MarketPolicy.PRODUCT_PAYOUT_RATE;
+
+    public OrderItem(Order order, Product product, String productName, long price, long salePrice) {
+        this.order = order;
+        this.product = product;
+        this.productName = productName;
+        this.price = price;
+        this.salePrice = salePrice;
+    }
+}

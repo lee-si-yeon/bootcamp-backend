105403e8ef2092b764ecb4a8aa067f6405fbdea0
leesiyeon <sylee4880@naver.com>
2026-09-22 11:03:33 +0900

    fix: 결제 요청 시 지연 로딩 오류 해결

--- src/main/java/com/back/boundedContext/market/app/MarketFacade.java
+++ src/main/java/com/back/boundedContext/market/app/MarketFacade.java
@@ -95,7 +95,7 @@ public class MarketFacade {
 
     @Transactional
     public void requestPayment(Order order, long pgPaymentAmount) {
-        order.requestPayment(pgPaymentAmount);
+        marketSupport.findOrderById(order.getId()).get().requestPayment(pgPaymentAmount);
     }
 
     @Transactional

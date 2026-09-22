bcc83b6aa5d2f791616fd4daf3c69ee9534299fc
leesiyeon <sylee4880@naver.com>
2026-09-22 15:12:32 +0900

    fix: 홀딩 지갑 설정값 주입 적용

--- src/main/java/com/back/boundedContext/cash/domain/CashPolicy.java
+++ src/main/java/com/back/boundedContext/cash/domain/CashPolicy.java
@@ -1,7 +1,9 @@
 package com.back.boundedContext.cash.domain;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class CashPolicy {
 
     public static int HOLDING_MEMBER_ID;

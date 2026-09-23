b36080557321f5efcf77809582ccec8b2e823b3d
leesiyeon <sylee4880@naver.com>
2026-09-23 11:02:57 +0900

    feat: 정산 완료와 예치금 지급 처리 추가

--- src/main/java/com/back/shared/member/domain/BaseMember.java
+++ src/main/java/com/back/shared/member/domain/BaseMember.java
@@ -30,4 +30,8 @@ public abstract class BaseMember extends BaseEntity {
         this.nickname = nickname;
         this.activityScore = activityScore;
     }
+
+    public boolean isSystem() {
+        return "system".equals(username);
+    }
 }
